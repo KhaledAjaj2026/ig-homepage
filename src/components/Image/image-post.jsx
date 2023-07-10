afd329d574b88e81b76f931343e324fce53cd312
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-10 16:54:58 -0700

    added media queries for mobile and tablet devices; stubborn space below image that will not disappear, must fix.

--- src/components/Image/image-post.jsx
+++ src/components/Image/image-post.jsx
@@ -5,7 +5,7 @@ export default function ImagePost() {
         <>
             <div id='image-post_component'>
                 <hr />
-                <img className='image-post' src="" alt="" width="auto" height="250" />
+                <img className='image-post' src="src/assets/prof-pic_4.jpg" alt="" />
                 <hr />
             </div>
         </>

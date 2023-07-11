f6fc3638a1ec45b5b1e3bc9d604802acd1ab20fb
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-11 15:47:10 -0700

    added two more buttons: share and save.

--- src/components/Interactions/interactions.jsx
+++ src/components/Interactions/interactions.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react';
 import './interactions.scss';
 
 export default function Interactions() {
-    const [count, setCount] = useState(0);
+    const [likes, setLikes] = useState(0);
+    const [shares, setShares] = useState(0);
+    const [save, setSave] = useState(false);
+
+    function savedPost() {
+        if (save === false) {
+            return "Unsaved";
+        } else {
+            return "Saved";
+        }
+    }
 
     return (
         <>
             <div id='interactions-component'>
-                <button className='like-button' onClick={() => setCount((count) => count + 1)}>❤️</button>
-                <p className="likes-count">{count} Likes</p>
+                <button className='like-button' onClick={() => setLikes((likes) => likes + 1)}>❤️</button>
+                <button className='share-button' onClick={() => setShares((shares) => shares + 1)}>❤️</button>
+                <button className='save-button' onClick={() => setSave((save) => save = !save)}>❤️</button>
+                <p className="likes-count">{likes} Likes</p>
+                <p className="share-count">{shares} Shares</p>
+                <p className="save-count">{savedPost()}</p>
             </div>
         </>
     )
